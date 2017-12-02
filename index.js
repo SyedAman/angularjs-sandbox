68b9cbf5ca25d47536d069d9c635de80e2a0f2ea
Syed Aman <sna274@nyu.edu>
2017-12-02 17:14:24 -0600

    create a module and controller

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var Home = function($scope) {
-  $scope.message = 'hello world';
-};
+var messWithAngular = angular.module('messWithAngular', []);
+
+messWithAngular.controller('HomeController', [
+  '$scope',
+  function($scope) {
+    $scope.message = 'hello world';
+  },
+]);
 
 console.log('hey');
